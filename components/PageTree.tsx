96730843661db596cb912d66b7532fa9934c3d0c
xonika9 <xonika9@gmail.com>
2023-05-02 17:40:17 +0300

    refactor tree

--- components/PageTree.tsx
+++ components/PageTree.tsx
@@ -17,31 +17,41 @@ interface PageTreeProps {
   movePage: (draggedId: number, newParentId: number | null) => void;
 }
 
-const renderPages = (
-  pages: Page[],
-  parent_id: number | null,
-  movePage: (draggedId: number, newParentId: number | null) => void
-) => {
-  return pages
-    .filter((page) => page.parent_id === parent_id)
-    .map((page) => (
-      <div key={page.id} className={styles.pageTreeNode}>
-        <PageTreeItem
-          id={page.id}
-          title={page.title}
-          parent_id={page.parent_id}
-          movePage={movePage}
-        />
-        <div className={styles.pageTreeChildren}>
-          {renderPages(pages, page.id, movePage)}
-        </div>
+interface PageTreeNodesProps {
+  pages: Page[];
+  parent_id: number | null;
+  movePage: (draggedId: number, newParentId: number | null) => void;
+}
+
+const PageTreeNodes: React.FC<PageTreeNodesProps> = ({
+  pages,
+  parent_id,
+  movePage,
+}) => {
+  const renderNode = (page: Page) => (
+    <div key={page.id} className={styles.pageTreeNode}>
+      <PageTreeItem
+        id={page.id}
+        title={page.title}
+        parent_id={page.parent_id}
+        movePage={movePage}
+      />
+      <div className={styles.pageTreeChildren}>
+        <PageTreeNodes pages={pages} parent_id={page.id} movePage={movePage} />
       </div>
-    ));
+    </div>
+  );
+
+  return (
+    <>{pages.filter((page) => page.parent_id === parent_id).map(renderNode)}</>
+  );
 };
 
 const PageTree: React.FC<PageTreeProps> = ({ pages, movePage }) => {
   return (
-    <div className={styles.pageTree}>{renderPages(pages, null, movePage)}</div>
+    <div className={styles.pageTree}>
+      <PageTreeNodes pages={pages} parent_id={null} movePage={movePage} />
+    </div>
   );
 };
 
